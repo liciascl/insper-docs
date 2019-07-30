c0a50b541db27d7cde54e0559f2f9f9eea8b6ee0
Licia Sales <40764768+liciascl@users.noreply.github.com>
2019-07-30 16:29:25 -0300

    Update Instala_infra_robotica.sh

--- Informatica/Robotica/Scripts/Instala_infra_robotica.sh
+++ Informatica/Robotica/Scripts/Instala_infra_robotica.sh
@@ -7,6 +7,7 @@ if [ $? -eq 0 ]
 then
     echo "as keys criptográficas que assinam o software do ROS foram configuradas com sucesso"
     sudo apt update
+    sudo apt-get install -y screen
     sudo apt-get install -y ros-melodic-joy ros-melodic-teleop-tools  ros-melodic-teleop-twist-keyboard ros-melodic-laser-proc ros-melodic-rgbd-launch  ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs ros-melodic-amcl ros-melodic-map-server ros-melodic-move-base ros-melodic-urdf ros-melodic-xacro ros-melodic-compressed-image-transport ros-melodic-rqt-image-view  ros-melodic-navigation ros-melodic-interactive-markers  ros-melodic-kobuki-ftdi ros-melodic-ar-track-alvar-msgs ros-melodic-teleop-tools python-rosinstall
     if [ $? -eq 0 ]
     then	
@@ -17,7 +18,7 @@ then
 		echo "As dependencias ROS do curso foram instaladas com sucesso"
 		sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-* gstreamer1.0-libav* gstreamer1.0-plugins* 
 	
-			   if [ $? -eq 0 ]
+			if [ $? -eq 0 ]
    	  		   then
 				echo "Os softwares para lidar com stream de vídeo vindo do robô foram instaladas com sucesso"
 				source /opt/ros/melodic/setup.bash
@@ -26,7 +27,6 @@ then
 				catkin_init_workspace
 				cd ~/catkin_ws
 				catkin_make
-				
 				   if [ $? -eq 0 ]
    	  		   	   then
 					echo "O ambiente de trabalho do ROS foi configurado com sucesso"
@@ -45,31 +45,30 @@ then
 						cd ~/catkin_ws/
 						catkin_make
 
-
 					        if [ $? -eq 0 ]
-   	  		   	                then
-							echo "os gits para controle do Turtlebot foram clonandos com sucesso dos repositórios da ROBOTIS"
-							./instalar_opencv_jupyter.sh							
+   	  		   	               then
+								echo "os gits para controle do Turtlebot foram clonandos com sucesso dos repositórios da ROBOTIS"
+								exec "~/insper-docs/Informatica/Robotica/Scripts/instalar_opencv_jupyter.sh"							
 							
-							if [ $? -eq 0 ]
-   	  		   	                	then
-							echo "o opencv no python3 foi instalado, vamos instalar no python2 agora"					
-							./instalar_opencv_python2.sh
-								
 								if [ $? -eq 0 ]
+   	  		   	                	then
+									echo "o opencv no python3 foi instalado, vamos instalar no python2 agora"					
+									exec "~/insper-docs/Informatica/Robotica/Scripts/instalar_opencv_python2.sh"
+									
+									if [ $? -eq 0 ]
 	   	  		   	                	then
-								 echo "o opencv foi instalado com sucesso no python2 tambem, agora vamos instalar o hector slam"
-								./instalar_hector_slam.sh
-								
+								 		echo "o opencv foi instalado com sucesso no python2 tambem, agora vamos instalar o hector slam"
+										exec "~/insper-docs/Informatica/Robotica/Scripts/instalar_hecto_slam.sh"
+										echo 0
+									fi
 								fi
 							fi
-
-
 						fi
 					fi
-				fi
-			fi
-		fi
+				fi				
+			fi		
+		fi				   
 	fi
 fi
 
+exit
